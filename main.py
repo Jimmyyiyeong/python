bf2ef46ec412d6703cbf1c2cd5f6c93a40e0b444
Jimmy <Jimmy.yi.yeong@gmail.com>
2025-10-27 04:15:29 +0100

    Testing

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ axe = Weapon("Good Axe", 100,120)
 bat = Weapon("Exceptionally Durable Baseball Bat", 10, 200, 1000, "Exceedingly Rare", 3)
 Jimmy = Character("Jimmy", 69, "male", "thug", bat)
 
-Jimmy.greeting()
-bat.weapon_info()
+#Jimmy.greeting()
+#bat.weapon_info()
 
+Jimmy.show_inventory()
+Jimmy.add_to_inventory(axe)
+Jimmy.show_inventory()
