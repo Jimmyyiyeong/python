5511eecc8c208d76cd1db8188a1baa73f440d839
Jimmy <Jimmy.yi.yeong@gmail.com>
2025-10-27 16:29:58 +0100

    Basic enemy attributes

---
+++ AdventureGame/Enemy.py
@@ -0,0 +1,8 @@
+from AdventureGame.Weapon import Weapon
+
+class Enemy:
+    def __init__(self, name, health=None, weapon=None):
+        self.name = name
+        self.health = health
+        self.max_health = health
+        self.weapon = weapon
