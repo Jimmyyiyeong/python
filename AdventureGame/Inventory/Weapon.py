afa127158cf6f0383f83ec3f49648aba3dc8fc60
Althea.Olofsson <althea.olofsson@gmail.com>
2025-10-27 20:32:38 +0100

    added typo fix on Weapon

--- AdventureGame/Inventory/Weapon.py
+++ AdventureGame/Inventory/Weapon.py
@@ -1,7 +1,7 @@
 from .Item import Item
 
 class Weapon(Item):
-    item_type = "weapon"
+    item_type = "Weapon"
     attributes = ["name", "min_damage", "max_damage", "durability", "range"]
     
     def __init__(self, name, min_damage, max_damage, durability = None, rarity = "Common", range = 1, sell_price = 0):
@@ -15,10 +15,10 @@ class Weapon(Item):
 
     def describe(self):
         return (
-            f"{self.name} (Type: {self.item_type}, Rarity: {self.rarity}, "
+            f"{self.name}, Type: {self.item_type}, Rarity: {self.rarity}, "
             f"Damage: {self.min_damage}-{self.max_damage}, "
-            f"Durability: {self.durability},"
+            f"Durability: {self.durability}, "
             f"Range: {self.range}, "
-            f"Sell Price: {self.sell_price})"
+            f"Sell Price: {self.sell_price} "
         )
 
