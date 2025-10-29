8ed309591d011b4fb4058d846e70da872c1c5d5f
Jimmy <Jimmy.yi.yeong@gmail.com>
2025-10-29 21:55:56 +0100

    Testing

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
 from AdventureGame import *
 
-axe = Weapon("Good Axe", None, 100,120)
-sword = Weapon("Ironfang", "Sword", 5, 10, None, "Rare", 2)
-bat = Weapon("Exceptionally Durable Baseball Bat","Club", 10, 200, 1000, "Exceedingly Rare", 3)
-Jimmy = Character("Jimmy", 69, "male", "thug", axe)
-
-potion = Consumables("Healthpot",None, None, False)
-
-bow = Weapon("Rhok'delar, Longbow of the Ancient Keeper","Bow", 89, 166, None, "Legendary", 4)
+Jimmy = Character("Jimmy", 69, "male", "thug", 100, Frostmourne)
 
 Jimmy.greeting()
 Jimmy.show_inventory()
-Jimmy.add_to_inventory(axe)
-Jimmy.add_to_inventory(sword)
-Jimmy.add_to_inventory(bow)
-Jimmy.add_to_inventory(potion)
+Jimmy.add_to_inventory(Sulfuras)
+Jimmy.add_to_inventory(Minor_health_potion)
 Jimmy.show_inventory()
-Jimmy.equip_weapon(1)
+Jimmy.health = 50
+Jimmy.use_item(2)
+Jimmy.use_item(1)
 Jimmy.show_inventory()
 Jimmy.greeting()
-
-sword = Weapon("Ironfang",None, 5, 10, None, "Rare", 2)
-print(sword.describe())
 
 print(Goblin.describe())
