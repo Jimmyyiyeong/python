d86ee53c75cc1daf87dcb5dd3a903f2f98d9387e
Jimmy <Jimmy.yi.yeong@gmail.com>
2025-10-29 21:58:23 +0100

    Default behavior for using items + docstring

--- AdventureGame/Inventory/Item.py
+++ AdventureGame/Inventory/Item.py
@@ -1,14 +1,21 @@
 class Item:
 
-    def __init__(self, name, item_type, rarity = "Common"):
+    def __init__(self, name, item_type, rarity="Common"):
+        """ Default item attributes """
         self.name = name
         self.item_type = item_type
         self.rarity = rarity
         
     def describe(self):
+        """ Description of item """
         return f"{self.name}, Type: {self.item_type}, Rarity: {self.rarity} "
 
     def __str__(self):
+        """ Returns describe function as a string """
         return self.describe()
+    
+    def use (self, user):
+        """ Default behavior for items (may be overridden) """
+        print(f"{self.name} cannot be used directly.")
     
     
