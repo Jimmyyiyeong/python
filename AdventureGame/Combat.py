c05bcfb8ec60db1a80328976cbb49e7a2a25a686
Jimmy <Jimmy.yi.yeong@gmail.com>
2025-10-30 01:37:13 +0100

    Player and enemy attack logic

--- AdventureGame/Combat.py
+++ AdventureGame/Combat.py
@@ -15,3 +15,17 @@ class Combat:
         else:
             print("No weapon equipped!")
         print(f"{self.enemy.name}'s health: {self.enemy.health}")
+
+    def player_attack(self):
+        """ Randomizes damage based on weapon min and max damage. Max is a builtin python function that returns the larger of two values"""
+        damage = random.randint(self.player.weapon.min_damage, self.player.weapon.max_damage)
+        self.enemy.health = max(0, self.enemy.health - damage)
+        print(f"You attack {self.enemy.name} for {damage} damage!\n")
+
+    def enemy_attack(self):
+        """ Randomizes damage based on weapon min and max damage. Also checks enemy health"""
+        if self.enemy.health <= 0:
+            return
+        damage = random.randomint(self.enemy.weapon.min_damage, self.enemy.weapon.max_damage)
+        self.player.health = max(0, self.player.health - damage)
+        print(f"{self.enemy.name} attacks you for {damage} damage!\n")
