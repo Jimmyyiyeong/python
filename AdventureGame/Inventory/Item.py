35a3d30aa90f2926090cf85bd1e23d0237a671ad
Jimmy <Jimmy.yi.yeong@gmail.com>
2026-04-22 17:07:23 +0200

    Added description option to item

--- AdventureGame/Inventory/Item.py
+++ AdventureGame/Inventory/Item.py
@@ -1,14 +1,18 @@
 class Item:
 
-    def __init__(self, name, item_type="Item", rarity=None):
+    def __init__(self, name, item_type="Item", rarity=None, description=""):
         """ Default item attributes """
         self.name = name
         self.item_type = item_type
         self.rarity = rarity if rarity is not None else "Common"
+        self.description = description
         
     def describe(self):
         """ Description of item """
-        return f"{self.name} | Type: {self.item_type} | Rarity: {self.rarity} "
+        base = f"{self.name} | Type: {self.item_type} | Rarity: {self.rarity} | "
+        if self.description:
+            base += f"{self.description}"
+        return base
 
     def __str__(self):
         """ Returns describe function as a string """
